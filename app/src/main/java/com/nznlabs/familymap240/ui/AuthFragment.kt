8b65448335d928f8cb8d6012bbf55e35854084db
Jacob Zinn <jacobpzinn@gmail.com>
2022-03-14 22:28:31 -0600

    added dependency to server shared classes and models

--- app/src/main/java/com/nznlabs/familymap240/ui/AuthFragment.kt
+++ app/src/main/java/com/nznlabs/familymap240/ui/AuthFragment.kt
@@ -1,6 +1,8 @@
 package com.nznlabs.familymap240.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +12,15 @@ import com.nznlabs.familymap240.R
 import com.nznlabs.familymap240.databinding.FragmentAuthBinding
 import com.nznlabs.familymap240.viewmodel.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import models.AuthToken
+import requests.LoginRequest
 import timber.log.Timber
+
 
 @AndroidEntryPoint
 class AuthFragment : BaseFragment(),
-View.OnClickListener{
+    View.OnClickListener,
+    TextWatcher {
 
     val viewModel: AuthViewModel by viewModels()
 
@@ -33,7 +39,39 @@ View.OnClickListener{
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.launchBtn.setOnClickListener(this)
+        initListeners()
+    }
+
+    fun initListeners() {
+        binding.loginBtn.setOnClickListener(this)
+        binding.registerBtn.setOnClickListener(this)
+
+        binding.serverHostInputTxt.addTextChangedListener(this)
+        binding.serverPortInputTxt.addTextChangedListener(this)
+        binding.usernameInputTxt.addTextChangedListener(this)
+        binding.passwordInputTxt.addTextChangedListener(this)
+        binding.firstNameInputTxt.addTextChangedListener(this)
+        binding.lastNameInputTxt.addTextChangedListener(this)
+        binding.emailInputTxt.addTextChangedListener(this)
+    }
+
+    fun checkFieldsForEmptyValues() {
+        val s1: String = binding.serverHostInputTxt.text.toString()
+        val s2: String = binding.serverPortInputTxt.text.toString()
+        val s3: String = binding.usernameInputTxt.text.toString()
+        val s4: String = binding.passwordInputTxt.text.toString()
+
+        binding.loginBtn.isEnabled = !(s1.isEmpty() || s2.isEmpty() || s3.isEmpty() || s4.isEmpty())
+
+        val s5: String = binding.firstNameInputTxt.text.toString()
+        val s6: String = binding.lastNameInputTxt.text.toString()
+        val s7: String = binding.emailInputTxt.text.toString()
+        val genderID: Int = binding.genderRadioGroup.checkedRadioButtonId
+        Timber.d("gender id " + genderID)
+
+        binding.registerBtn.isEnabled = !(s1.isEmpty() || s2.isEmpty() || s3.isEmpty() || s4.isEmpty() || s5.isEmpty() || s6.isEmpty() || s7.isEmpty())
+
+
     }
 
     override fun onDestroyView() {
@@ -41,13 +79,23 @@ View.OnClickListener{
         _binding = null
     }
 
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    override fun afterTextChanged(p0: Editable?) {
+        checkFieldsForEmptyValues()
+    }
+
+
     override fun onClick(v: View?) {
         when (v!!) {
-            binding.launchBtn -> {
+            binding.loginBtn -> {
                 try {
                     findNavController().navigate(R.id.action_authFragment_to_mapFragment)
                 } catch (e: NullPointerException) {
-                    Timber.e(e, "navigateToSessionsFragmentFromRecViewSelection: ERROR: Failed to navigate")
+                    Timber.e(
+                        e,
+                        "navigateToSessionsFragmentFromRecViewSelection: ERROR: Failed to navigate"
+                    )
                 }
             }
         }
--- app/src/main/java/com/nznlabs/familymap240/ui/MapFragment.kt
+++ app/src/main/java/com/nznlabs/familymap240/ui/MapFragment.kt
@@ -1,14 +1,73 @@
 package com.nznlabs.familymap240.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.nznlabs.familymap240.R
+import com.nznlabs.familymap240.databinding.FragmentMapBinding
+import com.nznlabs.familymap240.viewmodel.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MapFragment: BaseFragment(), OnMapReadyCallback {
 
 
-    override fun onMapReady(p0: GoogleMap) {
-        TODO("Not yet implemented")
+
+    private lateinit var mMap: GoogleMap
+
+    val viewModel: AuthViewModel by viewModels()
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // init map
+        binding.map.getFragment<SupportMapFragment>().getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in United States and move the camera
+        val us = LatLng(39.0, -100.0)
+        mMap.addMarker(MarkerOptions().position(us).title("Marker in middle of US"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(us))
+    }
+
+
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
