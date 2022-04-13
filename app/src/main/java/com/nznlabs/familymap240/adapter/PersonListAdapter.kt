156bfde957e22084e1afffe191e0b400ddbb2fff
Jacob Zinn <jacobpzinn@gmail.com>
2022-04-12 23:59:16 -0600

    sorting of events and persons for person fragment + halfway through expandable listview

--- app/src/main/java/com/nznlabs/familymap240/adapter/PersonListAdapter.kt
+++ app/src/main/java/com/nznlabs/familymap240/adapter/PersonListAdapter.kt
@@ -1,4 +1,139 @@
 package com.nznlabs.familymap240.adapter
 
-class PersonListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import com.nznlabs.familymap240.databinding.LayoutGroupItemBinding
+import com.nznlabs.familymap240.databinding.LayoutPersonListItemBinding
+import models.Event
+import models.Person
+import java.lang.IllegalArgumentException
+
+class PersonListAdapter(private val events: List<Event>, private val relatives: Map<String, Any?>) :
+    BaseExpandableListAdapter() {
+
+    companion object {
+        private const val EVENT_GROUP_POS = 0
+        private const val RELATIVE_GROUP_POS = 1
+    }
+
+    override fun getGroupCount(): Int {
+        return 2
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return when (groupPosition) {
+            EVENT_GROUP_POS -> {
+                events.size
+            }
+            RELATIVE_GROUP_POS -> {
+                relatives.size
+            }
+            else -> {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
+    override fun getGroup(groupPosition: Int): Any {
+        return when (groupPosition) {
+            EVENT_GROUP_POS -> {
+                "LIFE EVENTS"
+            }
+            RELATIVE_GROUP_POS -> {
+                "FAMILY"
+            }
+            else -> {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        when (groupPosition) {
+            EVENT_GROUP_POS -> {
+                return events[groupPosition]
+            }
+            RELATIVE_GROUP_POS -> {
+                return when (childPosition) {
+                    0 -> {
+                        relatives["father"] as Person
+                    }
+                    1 -> {
+                        relatives["mother"] as Person
+                    }
+                    2 -> {
+                        relatives["spouse"] as Person
+                    }
+                    else -> {
+                        val children = relatives["children"] as List<*>
+                        children[childPosition - 3] as Person
+                    }
+                }
+            }
+            else -> {
+                throw IllegalArgumentException()
+            }
+        }
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+
+        val itemBinding = LayoutGroupItemBinding.inflate(
+            LayoutInflater.from(parent?.context),
+            parent,
+            false
+        )
+
+        when (groupPosition) {
+            EVENT_GROUP_POS -> {
+                itemBinding.title.text = "LIFE EVENTS"
+            }
+            RELATIVE_GROUP_POS -> {
+                itemBinding.title.text = "FAMILY"
+            }
+            else -> throw IllegalArgumentException()
+        }
+
+        return itemBinding.title
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+
+        when (groupPosition) {
+            EVENT_GROUP_POS -> {
+                view = LayoutGroupItemBinding.inflate(
+                    LayoutInflater.from(parent?.context),
+                    parent,
+                    false
+                )
+                    itemBinding.title.text = "LIFE EVENTS"
+            }
+            RELATIVE_GROUP_POS -> {
+                itemBinding.title.text = "FAMILY"
+            }
+            else -> throw IllegalArgumentException()
+        }
+
+        return itemBinding.title
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/nznlabs/familymap240/ui/PersonFragment.kt
+++ app/src/main/java/com/nznlabs/familymap240/ui/PersonFragment.kt
@@ -6,6 +6,8 @@ import androidx.navigation.fragment.navArgs
 import com.nznlabs.familymap240.adapter.PersonListAdapter
 import com.nznlabs.familymap240.databinding.FragmentPersonBinding
 import com.nznlabs.familymap240.viewmodel.MainViewModel
+import models.Event
+import models.Person
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 
@@ -22,13 +24,49 @@ class PersonFragment : BaseFragment<FragmentPersonBinding>()  {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initListView()
+        viewModel.persons.value?.let {
+            val rootPerson: Person = it[args.personID]!!
+            initListView(rootPerson, it)
+        }
     }
 
 
-    private fun initListView() {
+    private fun initListView(rootPerson: Person, persons: MutableMap<String, Person>) {
+        val events: List<Event> = viewModel.personEvents.value?.get(rootPerson.personID)?.toList() ?: mutableListOf()
+        val sortedEvents = sortEvents(events)
+        val relatives = findRelatives(rootPerson, persons)
 
+        personAdapter = PersonListAdapter(events = sortedEvents, relatives = relatives)
     }
 
+    private fun sortEvents(events: List<Event>): List<Event> {
+
+        val birthEvents = events.filter { it.eventType == "birth" }
+        val otherEvents = events.filter {it.eventType != "birth" && it.eventType != "death"}.sortedBy { it.year }
+        val deathEvents = events.filter { it.eventType == "death" }
+
+        val sortedEvents = mutableListOf<Event>()
+        sortedEvents.addAll(birthEvents)
+        sortedEvents.addAll(otherEvents)
+        sortedEvents.addAll(deathEvents)
+        return sortedEvents
+    }
+
+    private fun findRelatives(rootPerson: Person, persons: MutableMap<String, Person>): Map<String, Any?> {
+        val relatives = mutableMapOf<String, Any?>()
+        relatives["father"] = persons[rootPerson.fatherID]
+        relatives["mother"] = persons[rootPerson.motherID]
+        relatives["spouse"] = persons[rootPerson.spouseID]
+        // checking for children
+        val children = mutableListOf<Person>()
+        for (person in persons) {
+            if (person.value.fatherID == rootPerson.personID || person.value.motherID == rootPerson.personID) {
+                children.add(person.value)
+            }
+        }
+        relatives["children"] = children
+
+        return relatives
+    }
 
 }
